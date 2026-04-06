e19ae87522b15ec73f70dfc9806a3831d9224624
Axel Leon <axeleon76@gmail.com>
2026-04-06 22:06:00 +0200

    Proyecto creado

---
+++ src/main/java/app/kuriobackend/KurioBackendApplication.java
@@ -0,0 +1,13 @@
+package app.kuriobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurioBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KurioBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/kuriobackend/KurioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.kuriobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurioBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
